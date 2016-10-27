47f744fa5fd84b1d7b35a77a89f99e55d5eedd3c
Griffin Mullins <griffin.mullins@capitalone.com>
2016-10-27 14:07:44 -0500

    Initial Commit

---
+++ wk6LabTests/wk6LabTests.swift
@@ -0,0 +1,36 @@
+//
+//  wk6LabTests.swift
+//  wk6LabTests
+//
+//  Created by Mullins, Griffin on 10/27/16.
+//  Copyright © 2016 Mullins, Griffin. All rights reserved.
+//
+
+import XCTest
+@testable import wk6Lab
+
+class wk6LabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
